c0867e3b8775c3e22464b0e89d285d629f92fb17
vanessa <vanessapcbarros@gmail.com>
2021-01-26 10:58:14 +0100

    feat: add listdish component

---
+++ src/components/DishList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="dishList">
+    <h1>List of Dishes</h1>
+  </div>
+</template>
+
+<script>
+import "../../listOfDish.json";
+export default {
+  name: "DishList",
+
+  data() {
+    return {
+      dishes: [
+        {
+          id: 1,
+          name: "bagel",
+          description: "ham and cheese",
+          price: 12,
+          quantity: 0,
+          image: "www.nowhere.org",
+        },
+        {
+          id: 2,
+          name: "bagel",
+          description: "ham and cheese",
+          price: 12,
+          quantity: 0,
+          image: "www.nowhere.org",
+        },
+        {
+          id: 3,
+          name: "bagel",
+          description: "ham and cheese",
+          price: 12,
+          quantity: 0,
+          image: "www.nowhere.org",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <DishList msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DishList from "@/components/DishList.vue";
+
+export default {
+  name: "Home",
+  components: {
+    DishList
+  }
+};
+</script>
