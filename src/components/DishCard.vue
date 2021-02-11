93d55aba761df33c4928732fdf8d25329e5d013e
vanessa <vanessapcbarros@gmail.com>
2021-02-11 08:53:39 +0100

    add dishcard component

---
+++ src/components/DishCard.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="dishCard item-card" :key="dish.id">
+    <div class="item-photo-wrapper">
+      <img class="item-photo" :src="dish.image" :alt="dish.name" />
+    </div>
+    <div class="item-details">
+      <h4 class="item-price">{{ dish.price }}</h4>
+      <h4 class="item-title">{{ dish.name }}</h4>
+      <p class="item-options">{{ dish.description }}</p>
+      <div class="item-picker" id="dish.id">
+        <button class="remove-product">-</button>
+        <p id="quantity">0</p>
+        <button class="add-product">+</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DishCard",
+  props: {
+    dish: Object,
+  },
+};
+</script>
+
+<style scoped>
+
+
+.item-card {
+  display: grid;
+  grid-template-columns: 40% 60%;
+  grid-template-rows: 160px;
+  gap: 20px;
+  justify-items: center;
+  align-items: center;
+  margin-bottom: 20px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+}
+
+.item-card .item-photo-wrapper {
+  grid-area: 1/1;
+  border-radius: 20px;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+
+.item-photo {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.item-details {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  font-family: "Raleway", sans-serif;
+  font-weight: bolder;
+}
+
+.item-details > * {
+  margin: 0;
+}
+
+.item-title {
+  margin-bottom: 5px;
+}
+.item-price {
+  color: #ff6900;
+}
+
+.item-options {
+  margin-bottom: 5px;
+  color: #b3b1b1;
+  font-size: small;
+  font-weight: normal;
+  white-space: nowrap;
+  overflow: hidden;
+}
+
+.item-picker {
+  display: flex;
+  width: 90px;
+  padding-top: 15px;
+}
+
+.item-picker button {
+  width: 30px;
+  padding: 10px;
+  border: none;
+  font-weight: bold;
+  color: #b4b4b4;
+  border-radius: 5px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  background-color: transparent;
+}
+
+.item-picker p {
+  display: block;
+  width: 30px;
+  margin: 0;
+  font-size: 1rem;
+  font-weight: bold;
+  text-align: center;
+  line-height: 30px;
+  background-color: #dfdbdb;
+  color: #292929;
+}
+</style>
--- src/components/DishList.vue
+++ src/components/DishList.vue
@@ -2,22 +2,23 @@
   <div class="dishList">
     <h1>Food App</h1>
 
-    <ul>
-      <li v-for="dish in dishes" :key="dish.id">
-        {{ dish.name }}
-      </li>
-    </ul>
+    <section class="restaurant-menu">
+      <dish-card v-for="dish in dishes" :dish="dish" :key="dish.id" />
+    </section>
   </div>
 </template>
 
 <script>
 import apiService from "../apiclient/apiservice";
+import DishCard from "./DishCard.vue";
+
 export default {
+  components: { DishCard },
   name: "DishList",
 
   data() {
     return {
-      dishes: []
+      dishes: [],
     };
   },
 
@@ -25,17 +26,21 @@ export default {
     async getAllDishes() {
       const response = await apiService.getAllDishes();
       this.dishes = response.data;
-    }
+    },
   },
 
   mounted() {
     this.getAllDishes();
-  }
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.restaurant-menu {
+  overflow-y: scroll;
+  margin-bottom: 60px;
+}
 h3 {
   margin: 40px 0 0;
 }
